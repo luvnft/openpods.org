1bde708ea04d1ccdc1c3cd7dd0ce3232cacf95c9
The Wizard of Hahz <info@luvnft.com>
2024-06-09 08:16:49 -0400

    updated

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -3,12 +3,12 @@ export default function NavBar() {
     return (
     <div className="navbar bg-base-100 fixed z-10 shadow-xl">
         <div className="flex-1">
-            <Link href="/" className="btn btn-ghost text-xl">OpenPods.org</Link>
+            <Link href="https://podcast.atl5d.com" className="btn btn-ghost text-xl">🎙️ Podcast</Link>
         </div>
         <div className="flex-none">
             <ul className="menu menu-horizontal px-1 gap-3">
                 <li><Link href="/search" className="btn bg-primary text-white">Search</Link></li>
-                <li><Link className="btn bg-secondary text-white" href="https://github.com/haydenzeller/openpods.org/" target="_blank">Contribute</Link></li>
+                <li><Link className="btn bg-secondary text-white" href="https://discord.com/servers/atl5d-1244450286337003520" target="_blank">Discord</Link></li>
             </ul>
         </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
       <div className="hero min-h-screen bg-base-300">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Welcome to OpenPods!</h1>
+            <h1 className="text-5xl font-bold">Welcome to the ATL5D 🎙️ Podcast</h1>
             <h2 className="py-6 text-2xl font-bold">
-              No accounts, No subscriptions, No ads.
+              No accounts, No subscriptions, No ads. Chat about podcasts in our Discord.
             </h2>
             <Link href="/topcharts" className="btn bg-primary hover:bg-base-100">View Top Charts</Link>
           </div>
